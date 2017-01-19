57d0d440d9e0349dc3b41bc15e24d24a8d0bd1fd
AndreCoelho <andre.luiz1987@gmail.com>
2017-01-19 21:06:38 -0200

    Create Project

---
+++ TutorialToolbarItem/TutorialToolbarItem/TutorialToolbarItem/Views/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TutorialToolbarItem.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnClickButtonOne(object sender, EventArgs e)
+        {
+            var blnResponse = await App.Current.MainPage.DisplayAlert("Título", "Coloque uma mensagem aqui..", "Ok", "Cancelar");
+
+            string strMessage = (blnResponse ? "Apertou Ok!" : "Apertou Cancelar");
+            await App.Current.MainPage.DisplayAlert("Título", strMessage, "Ok");
+        }
+
+        async void OnClickButtonTwo(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.DisplayAlert("Título", "Alert de confirmação", "Ok");
+        }
+
+        async void OnClickButtonThree(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.DisplayAlert("Título", "Pesquisa", "Ok");
+        }
+    }
+}
