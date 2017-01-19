01d05a9b4d61b27b7db8671feef0cdac43ba01dc
AndreCoelho <andre.luiz1987@gmail.com>
2017-01-19 18:31:49 -0200

    Create Project

---
+++ TutorialTabbedPage/TutorialTabbedPage/TutorialTabbedPage/Views/SecondPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TutorialTabbedPage.Views
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+        }
+
+        async void OnChangePage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ThirdPage());
+        }
+    }
+}
